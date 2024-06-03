3a30e9b4120ad62f1172ea7452e8f6bec082ff6a
mathvaillant <matheusvaillant505@gmail.com>
2024-06-03 11:38:25 +0100

    #3.1

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -2,12 +2,14 @@ import { userService } from '@/services/user';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useEffect, useState } from 'react';
 import { router } from 'expo-router';
+import { User } from '@/types/user';
 
 interface AuthContextProps {
   isLoggedIn: boolean;
   isLoadingAuth: boolean;
   authenticate: (authMode: "login" | "register", email: string, password: string) => Promise<void>;
   logout: VoidFunction;
+  user: User | null;
 }
 
 const AuthContext = React.createContext({} as AuthContextProps);
@@ -19,12 +21,18 @@ export function useAuth() {
 export function AuthenticationProvider({ children }: React.PropsWithChildren) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoadingAuth, setIsLoadingAuth] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     async function checkIfLoggedIn() {
       const token = await AsyncStorage.getItem('token');
-      setIsLoggedIn(!!token);
-      router.replace('(authed)');
+      const user = await AsyncStorage.getItem('user');
+
+      if (token && user) {
+        setIsLoggedIn(true);
+        setUser(JSON.parse(user));
+        router.replace('(authed)');
+      }
     }
 
     checkIfLoggedIn();
@@ -38,7 +46,12 @@ export function AuthenticationProvider({ children }: React.PropsWithChildren) {
 
       if (response) {
         setIsLoggedIn(true);
+
         await AsyncStorage.setItem('token', response.data.token);
+        await AsyncStorage.setItem('user', JSON.stringify(response.data.user));
+
+        setUser(response.data.user);
+
         router.replace('(authed)');
       }
     } catch (error) {
@@ -50,7 +63,10 @@ export function AuthenticationProvider({ children }: React.PropsWithChildren) {
 
   async function logout() {
     setIsLoggedIn(false);
+    setUser(null);
+
     await AsyncStorage.removeItem('token');
+    await AsyncStorage.removeItem('user');
   }
 
   return (
@@ -60,6 +76,7 @@ export function AuthenticationProvider({ children }: React.PropsWithChildren) {
         logout,
         isLoggedIn,
         isLoadingAuth,
+        user
       } }>
       { children }
     </AuthContext.Provider>
